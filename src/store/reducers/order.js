0fe6d3a03fe815e9d267f94643b86001ebbe1272
church001 <simonreuben23@gmail.com>
2018-09-26 04:30:05 +0100

    added the logic to the order action creator reducer files

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -8,14 +8,23 @@ initialState = {
 const reducer = ( state = initialState, action ) => {
 	switch(action.type){
 		case actionTypes.PURCHASE_BURGER_SUCCESS:
+			const  newOrder = {
+				...action.orderData,
+				id: action.orderId
+			}
 			return {
-
+				...state,
+				loading: false,
+				order: state.order.concat(newOrder)
 			};
 		case actionTypes.PURCHASE_BURGER_FIALURE:
 			return {
-
+				...state,
+				loading: false
 			};
 		default:
 			return state;
 	}
 };
+
+export default reducer;
